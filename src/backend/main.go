c1308adc2e5dfa865a346298a8dc3f84e83c5dc6
L4mbads <rizacal.mamen@gmail.com>
2025-05-08 01:31:12 +0700

    feat: scrap to database (postgres)

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,80 +1,67 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 
 	"github.com/gocolly/colly"
-)
-
-type ElementType string
-
-const (
-	Starting ElementType = "Starting"
-	Tier1    ElementType = "Tier1"
-	Tier2    ElementType = "Tier2"
-	Tier3    ElementType = "Tier3"
-	Tier4    ElementType = "Tier4"
-	Tier5    ElementType = "Tier5"
-	Tier6    ElementType = "Tier6"
-	Tier7    ElementType = "Tier7"
-	Tier8    ElementType = "Tier8"
-	Tier9    ElementType = "Tier9"
-	Tier10   ElementType = "Tier10"
-	Tier11   ElementType = "Tier11"
-	Tier12   ElementType = "Tier12"
-	Tier13   ElementType = "Tier13"
-	Tier14   ElementType = "Tier14"
-	Tier15   ElementType = "Tier15"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 type RecipeType struct {
 	Element     string
-	ImgUrl1     string
-	ImgUrl2     string
 	Ingredient1 string
 	Ingredient2 string
-	Type        ElementType
 }
 
-func getElementType(index int) ElementType {
+type ElementType struct {
+	Name     string
+	ImageUrl string
+	Type     string
+}
+
+func getElementType(index int) string {
 	switch index {
 	case 1:
-		return Starting
+		return "Starting"
 	case 2:
 		// Table[2] is special, we skip it (Ruins/Archeologist)
 		return ""
 	case 3:
-		return Tier1
+		return "Tier1"
 	case 4:
-		return Tier2
+		return "Tier2"
 	case 5:
-		return Tier3
+		return "Tier3"
 	case 6:
-		return Tier4
+		return "Tier4"
 	case 7:
-		return Tier5
+		return "Tier5"
 	case 8:
-		return Tier6
+		return "Tier6"
 	case 9:
-		return Tier7
+		return "Tier7"
 	case 10:
-		return Tier8
+		return "Tier8"
 	case 11:
-		return Tier9
+		return "Tier9"
 	case 12:
-		return Tier10
+		return "Tier10"
 	case 13:
-		return Tier11
+		return "Tier11"
 	case 14:
-		return Tier12
+		return "Tier12"
 	case 15:
-		return Tier13
+		return "Tier13"
 	case 16:
-		return Tier14
+		return "Tier14"
 	case 17:
-		return Tier15
+		return "Tier15"
 	default:
 		return ""
 	}
@@ -82,7 +69,44 @@ func getElementType(index int) ElementType {
 
 func main() {
 	url := "https://little-alchemy.fandom.com/wiki/Elements_(Little_Alchemy_2)"
-	var recipes []RecipeType
+
+	// connect to database
+	// db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// create table if it doesn't exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS recipes (id SMALLINT PRIMARY KEY, element TEXT, image1 TEXT, image2 TEXT, ingredient1 TEXT, ingredient2 TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS recipes")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("DROP TABLE IF EXISTS elements")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS elements (name TEXT, image_url TEXT, type TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS recipes (element TEXT, ingredient1 TEXT, ingredient2 TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create router
+	router := mux.NewRouter()
+	router.HandleFunc("/api/go/elements", getElements(db)).Methods("GET")
+	router.HandleFunc("/api/go/recipes", getRecipes(db)).Methods("GET")
+
+	// wrap the router with CORS and JSON content type middlewares
+	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
 
 	c := colly.NewCollector(colly.AllowedDomains("little-alchemy.fandom.com"))
 	tableIndex := 0
@@ -105,9 +129,19 @@ func main() {
 			}
 
 			elementCounter++
-			fmt.Printf("\nElement[%v]: %-10s | %s\n", elementCounter, element, elementType)
+			// fmt.Printf("\nElement[%v]: %-10s | %s\n", elementCounter, element, elementType)
 
-			// each recipe to the element generated
+			aTags := h.DOM.Find("td:nth-of-type(1) a")
+			imgUrl, _ := aTags.Eq(0).Find("img").Attr("data-src")
+
+			sqlStatement := `
+			INSERT INTO elements (name, image_url, type)
+			VALUES ($1, $2, $3)`
+			_, err = db.Exec(sqlStatement, element, imgUrl, elementType)
+			if err != nil {
+				panic(err)
+			}
+
 			h.ForEach("td:nth-of-type(2) li", func(_ int, li *colly.HTMLElement) {
 				recipeCounter++
 				aTags := li.DOM.Find("a")
@@ -116,8 +150,8 @@ func main() {
 					return
 				}
 
-				imgUrl1, _ := aTags.Eq(0).Find("img").Attr("data-src")
-				imgUrl2, _ := aTags.Eq(2).Find("img").Attr("data-src")
+				// imgUrl1, _ := aTags.Eq(0).Find("img").Attr("data-src")
+				// imgUrl2, _ := aTags.Eq(2).Find("img").Attr("data-src")
 				ingredient1 := strings.TrimSpace(aTags.Eq(1).Text())
 				ingredient2 := strings.TrimSpace(aTags.Eq(3).Text())
 
@@ -125,18 +159,15 @@ func main() {
 					return
 				}
 
-				r := RecipeType{
-					Element:     element,
-					ImgUrl1:     imgUrl1,
-					ImgUrl2:     imgUrl2,
-					Ingredient1: ingredient1,
-					Ingredient2: ingredient2,
-					Type:        elementType,
+				// Insert into recipes table
+				sqlStatement := `
+				INSERT INTO recipes (element, ingredient1, ingredient2)
+				VALUES ($1, $2, $3)`
+				_, err = db.Exec(sqlStatement, element, ingredient1, ingredient2)
+				if err != nil {
+					log.Printf("Error inserting recipe for element '%s': %v", element, err)
+					return
 				}
-				recipes = append(recipes, r)
-				fmt.Printf("Recipe[%v]: %s + %s\n", recipeCounter, r.Ingredient1, r.Ingredient2)
-				fmt.Printf("ImgUrl1: %s\n", r.ImgUrl1)
-				fmt.Printf("ImgUrl2: %s\n", r.ImgUrl2)
 
 			})
 		})
@@ -150,8 +181,89 @@ func main() {
 		fmt.Print(e.Error())
 	})
 
-	err := c.Visit(url)
+	err = c.Visit(url)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// start server
+	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
+}
+
+func getElements(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM elements")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		elements := []ElementType{}
+		for rows.Next() {
+			var u ElementType
+			if err := rows.Scan(&u.Name, &u.ImageUrl, &u.Type); err != nil {
+				log.Fatal(err)
+			}
+			elements = append(elements, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(elements)
+
+	}
+
+}
+
+func getRecipes(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM recipes")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		recipes := []RecipeType{}
+		for rows.Next() {
+			var u RecipeType
+			if err := rows.Scan(&u.Element, &u.Ingredient1, &u.Ingredient2); err != nil {
+				log.Fatal(err)
+			}
+			recipes = append(recipes, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(recipes)
+
+	}
+
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Check if the request is for CORS preflight
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass down the request to the next middleware (or final handler)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set JSON Content-Type
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
