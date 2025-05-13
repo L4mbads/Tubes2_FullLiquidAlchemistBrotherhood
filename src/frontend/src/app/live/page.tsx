2c5a338ec9cb2e9bf9c659b1d487f9400ac15f50
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-05-13 13:29:10 +0700

    chore: comment out live page

--- src/frontend/src/app/live/page.tsx
+++ src/frontend/src/app/live/page.tsx
@@ -1,56 +1,56 @@
-'use client';
-import { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic';
+// 'use client';
+// import { useEffect, useState } from 'react';
+// import dynamic from 'next/dynamic';
 
-import { RecipeNodeType } from '@/components/RecipeNode';
+// import { RecipeNodeType } from '@/components/RecipeNode';
 
-const RecipeFlow = dynamic(() => import('../../components/RecipeFlow'), {
-  ssr: false,
-});
+// const RecipeFlow = dynamic(() => import('../../components/RecipeFlow'), {
+//   ssr: false,
+// });
 
-const LiveRecipePage = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+// const LiveRecipePage = () => {
+//   const [messages, setMessages] = useState<string[]>([]);
 
-  const [recipeTree, setRecipeTree] = useState<RecipeNodeType | null>(null);
-  useEffect(() => {
-    // Establish an SSE connection
-    const eventSource = new EventSource("http://localhost:8000/api/go/liverecipe?element=Bullet&count=5&strategy=bfs");
+//   const [recipeTree, setRecipeTree] = useState<RecipeNodeType | null>(null);
+//   useEffect(() => {
+//     // Establish an SSE connection
+//     const eventSource = new EventSource("http://localhost:8000/api/go/liverecipe?element=Bullet&count=5&strategy=bfs");
 
-    // Listen for messages from the server
-    eventSource.onmessage = (event) => {
-      setMessages((prevMessages) => [event.data]);
-      // setRecipeTree(event.data)
-    };
-    eventSource.addEventListener("done", () => {
-      console.log("Recipe generation completed.");
-      eventSource.close();
-    });
-    // Handle errors
-    eventSource.onerror = () => {
-      console.error("Error occurred with SSE connection.");
-      eventSource.close();
-    };
+//     // Listen for messages from the server
+//     eventSource.onmessage = (event) => {
+//       setMessages((prevMessages) => [event.data]);
+//       // setRecipeTree(event.data)
+//     };
+//     eventSource.addEventListener("done", () => {
+//       console.log("Recipe generation completed.");
+//       eventSource.close();
+//     });
+//     // Handle errors
+//     eventSource.onerror = () => {
+//       console.error("Error occurred with SSE connection.");
+//       eventSource.close();
+//     };
 
-    // Cleanup the connection when the component unmounts
-    return () => {
-      eventSource.close();
-    };
-  }, []);
+//     // Cleanup the connection when the component unmounts
+//     return () => {
+//       eventSource.close();
+//     };
+//   }, []);
 
-  return (
-    <div>
-      <h1>Live Recipe Updates</h1>
-        <div className="absolute top-0">
-        {messages.map((message, index) => (
-          <p key={index}>{message}</p>
-        ))}
-      </div>
+//   return (
+//     <div>
+//       <h1>Live Recipe Updates</h1>
+//         <div className="absolute top-0">
+//         {messages.map((message, index) => (
+//           <p key={index}>{message}</p>
+//         ))}
+//       </div>
 
-      {/* <div>
-          <RecipeFlow tree={recipeTree}/>
-        </div> */}
-    </div>
-  );
-};
+//       {/* <div>
+//           <RecipeFlow tree={recipeTree}/>
+//         </div> */}
+//     </div>
+//   );
+// };
 
-export default LiveRecipePage;
+// export default LiveRecipePage;
